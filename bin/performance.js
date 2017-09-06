b52f23b337bbc9585d380086fe07672d57a014a5
DenQ <denquick@gmail.com>
2017-09-06 22:15:26 +0300

    Performance with 'iron-tree' package

--- bin/performance.js
+++ bin/performance.js
@@ -1,4 +1,5 @@
 var LTT = require('../dist/list-to-tree.npm.js');
+const IronTree = require('@denq/iron-tree');
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,7 +13,7 @@ function performanceCalc(fn, ...params) {
     console.log(`Result: ${result}. Execution Time: ${end - start} ms`)
 }
 
-const list = (new Array(20000))
+const list = (new Array(1000))
   .fill()
   .map((item, index) => {
     return {
@@ -21,12 +22,27 @@ const list = (new Array(20000))
     }
   });
 
-function run() {
+
+function runListToTree() {
   var ltt = new LTT(list, {
     key_id: 'id',
-    key_parent: 'parent'
+    key_parent: 'parent',
+    key_child: 'children',
   });
   var tree = ltt.GetTree();
+  return 'list-to-tree';
 }
 
-performanceCalc(run);
+function runIronTree() {
+  const tree = new IronTree({ id: 0 });
+  list.forEach((item, index) => {
+    tree.add((parentNode) => {
+      return parentNode.get('id') === item.parent;
+    }, item);
+  });
+  const jTree = tree.toJson();
+  return 'iron-tree'
+}
+
+performanceCalc(runListToTree);
+performanceCalc(runIronTree);
